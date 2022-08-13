3a39fc96600d831c89cf80cc5dfd1ff64ba86a20
Sohyeon <soso981227@naver.com>
2022-08-13 21:21:44 +0900

    1-2.네비게이션 기능 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../routes/Login";
 import Home from "../routes/Home";
-import navbar from "./nav_bar";
+import Mypage from "../routes/Mypage";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLogin }) => (
   <Router>
+    {isLogin && <Navigation />}
     <Routes>
       {isLogin ? (
-        <Route path="/" element={<Home />}></Route>
+        <>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Mypage" element={<Mypage />}></Route>
+        </>
       ) : (
         <Route path="/" element={<Login />}></Route>
       )}
