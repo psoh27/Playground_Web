ac0b161bcc603346676bc12a234bac8fc3d1a1bf
Sohyeon <soso981227@naver.com>
2022-08-22 16:12:45 +0900

    1-3.리뷰 수정 및 삭제 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,19 @@ import { authService } from "../fb_init";
 function App() {
   const [initial, setInit] = useState(false);
   const [isLogin, setisLogin] = useState(authService.currentUser);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setisLogin(true);
+        setUserObj(user);
       } else {
         setisLogin(false);
       }
       setInit(true);
     });
   }, []);
-  return <AppRouter isLogin={isLogin} />;
+  return <AppRouter isLogin={isLogin} userObj={userObj} />;
 }
 
 export default App;
