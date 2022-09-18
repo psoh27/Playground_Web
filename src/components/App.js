ec7ad35ea8a14b69eb4d96fe6591fd64f7e06e4c
Sohyeon <soso981227@naver.com>
2022-09-18 11:22:06 +0900

    2.Mypage 내에서 프로필 수정하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import AppRouter from "./Router";
 import React, { useState, useEffect } from "react";
 import { authService } from "../fb_init";
+import { updateProfile } from "firebase/auth";
 
 function App() {
   const [initial, setInit] = useState(false);
@@ -10,14 +11,28 @@ function App() {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setisLogin(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (arg) =>
+            updateProfile(user, { displayName: user.displayName }),
+        });
+        if (user.displayName === null) {
+          const name = user.email.split("@")[0];
+          user.displayName = name;
+        }
       } else {
         setisLogin(false);
       }
       setInit(true);
     });
   }, []);
-  return <AppRouter isLogin={isLogin} userObj={userObj} />;
+  const refreshUser = () => {
+    setUserObj(authService.currentUser);
+  };
+  return (
+    <AppRouter refreshUser={refreshUser} isLogin={isLogin} userObj={userObj} />
+  );
 }
 
 export default App;
