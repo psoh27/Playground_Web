f66f36c49847767340d36917b38d6a5a428d2d04
Sohyeon <soso981227@naver.com>
2022-09-18 11:28:44 +0900

    2.Mypage에 유저 아이디 연동

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = () => (
+const Navigation = ({ userObj }) => (
   <nav>
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/Mypage">My page</Link>
+        <Link to="/Mypage">{userObj.displayName}님의 페이지</Link>
       </li>
     </ul>
   </nav>
